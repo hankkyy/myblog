840e27acf9c219c6cf446ad7984ebb7d7a3044bc
hankkyy <>
2026-06-17 14:28:19 +0800

    change author name to Zihao Zhang

--- src/build.py
+++ src/build.py
@@ -14,7 +14,7 @@ THEME = SRC / "themes" / "cenote-style"
 CSS_DIR = ROOT / "css"
 
 SITE = {"title":"纵横四海","url":"https://hankzhang.us/","desc":"但行好事，莫问前程"}
-AUTHOR = "张子豪"
+AUTHOR = "Zihao Zhang"
 YEAR = str(datetime.now().year)
 MENU = [("首页","/"),("文章列表","/posts/"),("联系","/guestbook/"),("关于","/about/"),("GitHub","https://github.com/hankkyy")]
 SAFE = {".git","src","vercel.json",".gitignore","README.md","pagefind"}
